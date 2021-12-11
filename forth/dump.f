32ad44e994e4e40911ebf6ab1a9792073011a196
petrihakkinen <petrih3@gmail.com>
2021-12-11 12:38:35 +0200

    Better version of dump.f

--- forth/dump.f
+++ forth/dump.f
@@ -1,15 +1,15 @@
-CREATE TABLE 32 ALLOT
+16 BASE C! ( Print and parse all numbers as hex )
 
-16 BASE C!
-
-: DUMP
+: DUMP ( address count -- )
 	0 DO
-		I 7 AND 0= IF CR THEN
-		DUP I + C@
-		DUP 10 < IF ASCII 0 EMIT THEN 
+		I 7 AND 0= IF CR THEN ( Line break every 8 bytes )
+		DUP I + C@ ( Fetch byte )
+		DUP 10 < IF ASCII 0 EMIT THEN  ( Prefix with "0" if byte is less than 10 in hex )
 		.
 	LOOP ;
 
-INVIS CLS
-3c51 40 DUMP
+( Test code to dump )
+: HELLO ." WORLD" ;
 
+INVIS CLS
+FIND HELLO 40 DUMP
