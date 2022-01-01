4da4956000b5c49b2f6fa084dc2324ad25919ac5
petrihakkinen <petrih3@gmail.com>
2022-01-01 14:49:46 +0200

    Added mcode words 0= 0> 0<. Comments are now supported inside mcode definitions.

--- mcode.f
+++ mcode.f
@@ -14,6 +14,36 @@
 
 :m arith 3 4 +   3 4 -   5 1+   5 1- ;
 
+:m rel-ops
+	0 0= ( 1 )
+	256 0= ( 0 )
+
+	-123 0< ( 1 )
+	0 0< ( 0 )
+	123 0< ( 0 )
+
+	5 0> ( 1 )
+	0 0> ( 0 )
+	-5 0> ( 0 ) ;
+
+: time ( -- time )
+	252 in ( lo byte )
+	253 in ( hi byte )
+	256 * + ;
+
+: begin-profile ( -- start-time ) time ;
+
+: end-profile ( start-time -- ) time swap - ." RESULT: " . ;
+
+( 6276 -> 2381 = 2.6 times faster! )
+:m speed-test
+	10000
+	begin
+		1-
+		dup 0=
+	until
+	drop ;
+
 : dump ( address count -- )
 	16 base c!
 	0 do
@@ -31,4 +61,6 @@ find test 2+ 10 dump
 	cr push8 . ( prints 8 )
 	cr test-begin-until ( prints "*AAAAA*" )
 	cr arith . . . . ( prints 4 6 -1 7 )
+	cr rel-ops . . . . . . . . ( prints 0 0 1 0 0 1 0 1 )
+	cr begin-profile speed-test end-profile
 	;
