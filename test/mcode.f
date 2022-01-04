68ec5b38d36e084e2e7e48bfb074aa993025e054
petrihakkinen <petrih3@gmail.com>
2022-01-04 16:16:59 +0200

    Refactored test/mcode.f into proper test suite.

--- test/mcode.f
+++ test/mcode.f
@@ -7,30 +7,147 @@ decimal
 code di 243 c, 253 c, 233 c, 
 code ei 251 c, 253 c, 233 c, 
 
+: fail ." FAILED!" abort ;
+
+: chk ( result expected -- )
+	= if ." OK" else fail then ;
+
+: chk2 ( result1 result2 expected1 expected2 -- )
+	rot = >r ( s: result1 expected1 ; r: eq2 )
+	= r> ( s: eq1 eq2 )
+	and if ." OK" else fail then ;
+
+: chk3 ( result1 result2 result3 expected1 expected2 expected3 -- )
+	4 roll = >r ( s: result1 result2 expected1 expected2 ; r: eq3 )
+	3 roll = >r ( s: result1 expected1 ; r: eq3 eq2 )
+	= r> r> ( s: eq1 eq2 eq3 )
+	and and if ." OK" else fail then ;
+
 :m stack
-	123 dup . . ( 123 123 )
-	456 123 drop . ( 456 )
-	45 67 over . . . ( 45 67 45 )
-	72 ?dup . . ( 72 72 )
-	0 ?dup 1 . . ( 1 0 )
+	." DUP    " 123 dup          123 123 chk2 cr
+	." DROP   " 123 456 drop     123 chk cr
+	." OVER   " 123 456 over     123 456 123 chk3 cr
+	." ?DUP   " 123 ?dup         123 123 chk2 space
+	            123 0 ?dup 456   123 0 456 chk3 cr
+	." PICK   " 3 4 2 pick       3 4 3 chk3 cr
+	." ROLL   " 1 2 2 roll       2 1 chk2 space
+	            1 2 3 3 roll     2 3 1 chk3 cr
+	." >R R>  " 3 7 >r           3 chk space r> 7 chk cr
 	;
 
-:m stack2
-	3 4 2 pick . . . ( 3 4 3 )
-	1 2 2 roll . . ( 1 2 )
-	1 2 3 3 roll . . . ( 1 3 2 )
-	3 7 >r . r> . ( 3 7 )
+:m arith
+	." +      " 3 4 +            7 chk cr
+	." -      " 3 4 -            -1 chk cr
+	." 1+     " 5 1+             6 chk cr
+	." 1-     " 5 1-             4 chk cr
+	." NEGATE " 1234 negate      -1234 chk space
+	            -1234 negate     1234 chk cr
+	." *      " 1000 5 *         5000 chk space
+	            -123 5 *         -615 chk cr
+	." C*     " 5 50 c*          250 chk cr
+	." ABS    " 4892 abs         4892 chk space
+	            -4892 abs        4892 chk cr
+	." MIN    " 7000 123 min     123 chk space
+	            -7000 123 min    -7000 chk cr
+	." MAX    " 7000 123 max     7000 chk space
+	           -7000 123 max     123 chk cr
 	;
 
-( Test calling another mcode word from mcode word )
-:m mc-push1 1 ;
-:m call-mcode mc-push1 . ;
+:m boolean-ops
+ 	." XOR    " 10123 2063 xor   12164 chk cr
+ 	." AND    " 11131 1241 and   89 chk cr
+ 	." OR     " 7072 32120 or    32760 chk cr
+ 	;
 
-( Test calling another user defined Forth word from mcode word )
+:m rel-ops
+	." 0=     " 0 0=             1 chk space
+	            256 0=           0 chk cr
+
+	." 0<     " -123 0<          1 chk space
+	            0 0<             0 chk space
+	            123 0<           0 chk cr
+
+	." 0>     " 5 0>             1 chk space
+	            0 0>             0 chk space
+	            -5 0>            0 chk cr
+
+	." =      " 3 5 =            0 chk space
+	            12345 12345 =    1 chk space
+	            -12345 12345 =   0 chk cr
+
+	." >      " 3 5 >            0 chk space
+	            5 -3 >           1 chk space
+	            0 0 >            0 chk cr
+
+	." <      " 3 5 <            1 chk space
+	            5 -3 <           0 chk space
+	            0 0 <            0 chk cr
+	;
+
+( Bug: this test fails if compiled to machine code! )
+:m test-again-new
+	1
+	begin
+		dup +			
+		dup 1000 > if exit then
+	again ;
+
+:m test-loop
+	0
+	2 0 do
+		5 0 do
+			i j + +
+		loop
+	loop ;
+
+:m test-goto
+	123 >r
+	3
+	label back
+		r> 1- >r
+		1-
+	?dup if goto back then
+	r> ;
+
+:m control-flow
+	." IF     " 1 2 if 3 then             1 3 chk2 space
+	            1 0 if 2 else 3 then      1 3 chk2 cr
+
+	." UNTIL  " 6 1 0 0 begin 5 >r until  r> r> r> 5 5 5 chk3 space 6 chk cr
+	." AGAIN  " test-again-new            1024 chk cr
+	." LOOP   " 0 1000 0 do i + loop      -24788 chk space
+	            test-loop                 25 chk cr
+	." GOTO   " 5 goto skip 6 label skip  5 chk space ( Forward goto )
+	            test-goto                 120 chk cr ( Backward goto )
+	;
+
 : push1 1 ;
-:m call-forth push1 . ;
 
-:m in-out
+:m push1-mc 1 ;
+
+:m inter-op
+	." FCALL  " push1            1 chk cr ( Call Forth word from mcode word )
+	." MCALL  " push1-mc         1 chk cr ( Call mcode word from mcode word )
+	;
+
+0 variable v
+
+( Bug: Undefined word 'v' error if this is compiled to machine code! )
+: mem
+	." ! @    " 12345 v ! v @    12345 chk cr
+	." C! C@  " 123 v c! v c@    123 chk cr
+	;
+
+:m print
+	." EMIT   " ascii * emit cr
+	." SPACE  " space ascii * emit cr
+	." SPACES "	2 spaces ascii * emit cr
+	( 10 10 at )
+	." TYPE   " 15424 5 type space ." RULES" cr
+	;
+
+:m test-in-out
+	." PLAYING SOUND..."
 	12345
 	100 begin
 		65278 in drop ( pull speaker )
@@ -38,129 +155,22 @@ code ei 251 c, 253 c, 233 c,
 		0 65278 out ( push speaker )
 		300 0 do loop
 		1- dup 0=
-	until drop . ;
-
-:m arith
-	3 4 + . ( 7 )
-	3 4 - . ( -1 )
-	5 1+ . ( 6 )
-	5 1- . ( 4 )
-	1234 negate . ( -1234 )
-	-1234 negate . ( 1234 )
-	1000 5 * . ( 5000 )
-	-123 5 * . ( -615 )
-	5 50 c* . ( 250 )
-	;
-
-:m arith-funcs
-	4892 abs . ( 4892 )
-	-4892 abs . ( 4892 )
-	7000 123 min . ( 123 )
-	-7000 123 min . ( -7000 )
-	7000 123 max . ( 7000 )
-	-7000 123 max . ( 123 )
-	;
-
-:m boolean-ops
- 	10123 2063 xor . ( 12164 )
- 	11131 1241 and . ( 89 )
- 	7072 32120 or . ( 32760 )
- 	;
-
-:m rel-ops
-	0 0= . ( 1 )
-	256 0= . ( 0 )
-
-	-123 0< . ( 1 )
-	0 0< . ( 0 )
-	123 0< . ( 0 )
-
-	5 0> . ( 1 )
-	0 0> . ( 0 )
-	-5 0> . ( 0 )
-
-	3 5 = . ( 0 )
-	12345 12345 = . ( 1 )
-	-12345 12345 = . ( 0 )
-
-	3 5 > . ( 0 )
-	5 -3 > . ( 1 )
-	0 0 > . ( 0 )
-
-	3 5 < . ( 1 )
-	5 -3 < . ( 0 )
-	0 0 < . ( 0 )
-	;
-
-:m mem 
-	SCREEN c@ ( read char 'm' )
-	SCREEN 20 + c! ( write char )
-	SCREEN @ ( read short 'ma' )
-	SCREEN 24 + ! ( write short )
-	;
-
-:m print
-	cr ascii * emit
-	cr space ascii * emit
-	cr space space ascii * emit
-	cr 3 spaces ascii * emit
-	10 10 at
-	15424 5 type ( print "FORTH" )
-	space ." RULES"
-	;
-
-:m test-base
-	16 base c! 255 . ( ff )
-	decimal 255 . ( 255 )
-	;
+	until drop ;
 
 :m test-inkey 
+	." *PRESS SPACE* "
 	begin
-		cr inkey .
-	0 until ;
+		inkey 32 =
+	until ;
 
-:m if-then
-	1 if ." TRUE " then
-	0 if ." TRUE" else ." FALSE" then
+:m misc
+	." LIT    " [ 5 3 * ] lit      15 chk cr
+	." CONST  " SCREEN             9216 chk cr
+	." BASE   " 16 base c! 255 . cr ( ff )
+	." DECIMAL " decimal 255 . cr ( 255 )
+	." INKEY   " test-inkey cr
+	." IN OUT  " test-in-out cr    12345 chk cr
 	;
-
-:m begin-until
-	ascii * emit
-	1 0 0 0 0
-	begin
-		ascii A emit
-	until 
-	ascii * emit ;
-
-:m begin-again
-	begin
-		ascii * emit
-	again ;
-
-:m do-loop
-	2 0 do
-		5 0 do
-			i j + .
-		loop
-	loop
-	100 0 do
-		ascii * emit leave
-	loop ;
-
-:m test-goto
-	( Forward jump )
-	goto skip
-	." NOT PRINTED"
-	label skip
-	." HERE" 
-	( Backward jump )
-	3
-	label back
-	1- dup .
-	?dup if goto back then
-	;
-
-:m test-lit [ 5 3 * ] lit . ;
 
 ( 11986 -> 4714, 2.5 times faster )
 :m benchmark-stack
@@ -229,28 +239,19 @@ code ei 251 c, 253 c, 233 c,
 	decimal ; immediate
 
 : main
-	fast di
-	cr call-forth call-mcode
-	cr stack
-	cr stack2
-	cr arith ( prints 4 6 -1 7 )
-	cr arith-funcs
-	cr boolean-ops ( prints 12164 89 32760 )
-	cr rel-ops ( prints 1 0 1 0 0 1 0 0 )
-	\ cr begin-profile speed-test end-profile
+	fast di cls invis
+	stack
+	arith
+	boolean-ops
+	rel-ops
+	control-flow
+	inter-op
+	mem
+	print
+	misc
+	cr ." ALL TESTS PASSED!" 
 	\ cr begin-profile benchmark-stack end-profile
 	\ cr begin-profile benchmark-over end-profile
 	\ cr begin-profile benchmark-do-loop end-profile
 	\ cr begin-profile benchmark-c* end-profile
-	\ in-out
-	cr test-base
-	mem
-	cr if-then ( prints TRUE FALSE )
-	cr test-goto ( prints HERE )
-	cr test-lit ( prints 15 )
-	cr begin-until ( prints *AAAAA* )
-	cr cr do-loop ( prints 0 1 2 3 4 1 2 3 4 5 * )
-	\ begin-again
-	cr print
-	\ test-inkey
 	;
