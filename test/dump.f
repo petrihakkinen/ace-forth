46480a8d7c759826968a9544ed0a93e2a1d90bbe
petrihakkinen <petrih3@gmail.com>
2022-04-20 20:54:40 +0300

    Fixed test/dump.f

--- test/dump.f
+++ test/dump.f
@@ -1,15 +1,14 @@
-16 base c! ( Print and parse all numbers as hex )
+( Program for printing the contents of memory )
+( For example, the following dumps the first 32 bytes of the word HELLO: FIND HELLO 32 DUMP )
 
 : dump ( address count -- )
+	hex
 	0 do
 		i 7 and 0= if cr then ( Line break every 8 bytes )
 		dup i + c@ ( Fetch byte )
-		dup 10 < if ascii 0 emit then  ( Prefix with "0" if byte is less than 10 in hex )
+		dup 16 < if ascii 0 emit then  ( Prefix with "0" if byte is less than 10 in hex )
 		.
 	loop ;
 
 ( Test code to dump )
 : hello ." world" ;
-
-invis cls
-find hello 40 dump
